152926f067cfe96b4cf4f6fba03cd4182bdb84f8
Spyros Argalias <sargalias@gmail.com>
2018-04-14 21:16:38 +0100

    add delete ajax on articles/show view

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation()
+
+const deleteArticleBtn = document.getElementById('js-delete-article');
+if (deleteArticleBtn) {
+    deleteArticleBtn.addEventListener('click', deleteArticle);
+}
+
+function deleteArticle(e) {
+    // Create ajax delete request to URL with article.id
+    // Get the article._id from e.target data attribute.
+    // Redirect to data-redirect attribute value
+    const xhr = new XMLHttpRequest();
+    const articleId = e.target.getAttribute('data-article-id');
+    const redirectTarget = e.target.getAttribute('data-redirect');
+    xhr.onload = function(e) {
+        if (this.status === 200) {
+            window.location.replace(redirectTarget);
+        }
+        else {
+            alert(`${this.status} - ${this.responseText}`);
+        }
+    };
+    xhr.open('DELETE', `/articles/${articleId}`);
+    xhr.send();
+}
